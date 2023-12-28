d60cfd9593383771b5d47235a28dba445fb8322e
hugepower <yezicccccc@gmail.com>
2023-12-28 12:45:28 +0800

    添加字数显示的功能

--- Sidebar/MyDraft/popup.js
+++ Sidebar/MyDraft/popup.js
@@ -1,12 +1,17 @@
 // 获取HTML元素
-const textbox = document.getElementById('main-textarea');
+const topTextbox = document.getElementById('top-textarea');
+const mainTextbox = document.getElementById('main-textarea');
 const copyButton = document.getElementById('copyButton');
 const clearButton = document.getElementById('clearButton');
+
+mainTextbox.addEventListener('input', function () {
+  topTextbox.value = mainTextbox.value.length;
+});
 
 // 当点击“拷贝”按钮时，复制文本区域中的内容到剪贴板
 copyButton.addEventListener('click', () => {
   navigator.clipboard
-    .writeText(textbox.value)
+    .writeText(mainTextbox.value)
     .then(() => {
       //alert('已复制文本到剪贴板！'); // 提示用户
     })
@@ -17,7 +22,8 @@ copyButton.addEventListener('click', () => {
 
 // 当点击“清空”按钮时，清空文本区域中的内容
 clearButton.addEventListener('click', () => {
-  textbox.value = '';
+  mainTextbox.value = '';
+  topTextbox.value = '';
 });
 
 document.addEventListener('DOMContentLoaded', function () {
